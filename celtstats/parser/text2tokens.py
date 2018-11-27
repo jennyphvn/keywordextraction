ead3463ed877854fc03347edfab1a97923582bf7
Katherine Hasker <haskerk@stthom.edu>
2018-11-27 15:45:22 -0600

    typo

--- celtstats/parser/text2tokens.py
+++ celtstats/parser/text2tokens.py
@@ -41,7 +41,7 @@ def text2tokens(stream) :
 # https://stackoverflow.com/questions/3939361/remve-specific-characters-from-a-string-in-python
 # https://stackoverflow.com/questions/5843518/remove-all-special-characters-punctuation-and-spaces-from-string
 def useRegularExpression(stream, option='alphanum') :
-    newStrem = stream
+    newStream = stream
     if option== 'alphanum' :
         newStream = re.sub(r'[^a-zA-Z0-9 ]',r'',stream)
     elif option == 'alpha' :
