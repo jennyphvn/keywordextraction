0a377ce68b0e746d5bc79e2cd82ae7e4f68bf856
Jenny Phan <phanjh@stthom.edu>
2018-12-04 23:05:25 -0600

    Dictionary 1

--- celtstats/builder/__init__.py
+++ celtstats/builder/__init__.py
@@ -15,9 +15,17 @@ Main Features
 print('loading celtstats.builder module __init__.py')
 #from .building import *
 
-documentIndex = {}
+documentIndex = { 'document_1122':
+                    ( 38,
+                        { 'first' : 1, 'time' : 1, 'google' : 1, 'photos' : 1, 'made' : 1, 'me' : 2, 'cry' : 1, 'sucker' : 1, 'punch' : 1, 'had' : 2, 'looked' : 1, 'my' : 2, 'phone' : 1, 'one' : 1, 'morning' : 1, 'april' : 1, 'expecting' : 1, 'more' : 1, 'news' : 1, 'global' : 1, 'woe' : 1, 'instead' : 1, 'there' : 1, 'alert' : 1, 'photos' : 1, 'letting' : 1, 'know' : 1, 'googles' : 1, 'image-processing' : 1, 'robots' : 1, 'created' : 1, 'some' : 1, 'kind' : 1, 'montage' : 1, 'videos' : 1}),
+                  'document_3456':
+                    ( 29,
+                        { 'attract' : 1, 'amazon' : 2, 'new' : 1, 'york' : 1, 'city' : 2, 'leaders' : 1, 'agreed' : 1, 'remake' : 1, 'plans' : 1, 'queens' : 1, 'waterfront' : 1, 'move' : 1, 'distribution' : 1, 'center' : 1, 'school' : 1, 'lunches' : 1, ' provide' : 1, ' sweeping' : 1, 'package' : 1, 'billion' : 1, ' incentives' : 1, 'state' : 1, 'and' : 1, 'hundreds' : 1, 'millions' : 1, 'more' : 1}),
+                  'document_7788':
+                    ( 40,
+                        { 'paradise' : 2, 'old' : 1, 'gold' : 'rush' : 1, 'town' : 1, 'tucked' : 1, ' sierra' : 1, 'nevada' : 1, 'foothills' : 1, 'lies' : 1, 'miles' : 2, ' north' : 1, 'sacramento' : 1, 'and' : 2, 'less' : 1, 'than' : 1, 'here' : 1, 'sister' : 1, 'communities' : 1, 'connected' : 1, 'skyway' : 1, 'four' : 1, 'lane' : 1, 'winding' : 1, 'road' : 1, 'its' :1, 'daily' : 1, 'commute' : 1, 'thousands' : 1, 'sole' : 1, 'route' : 1, 'out' : 1, 'into' : 1, 'chico' : 1, 'nearest' : 1, 'urban' : 1, 'area' : 1 })
 #for
 #def buildDiction(document, words):
-#    for w in words :
+#    for w in words :''
 #        documentIndex[document] = words
 #one is going to be the key. one os going to be the value
