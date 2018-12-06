8b536c502e909ab891ca1e8a19a02a226ba87b56
Jenny Phan <phanjh@stthom.edu>
2018-12-05 20:56:19 -0600

    d

--- celtstats/builder/__init__.py
+++ celtstats/builder/__init__.py
@@ -24,6 +24,339 @@ documentIndex = { 'document_1122':
                   'document_7788':
                     ( 40,
                         { 'paradise' : 2, 'old' : 1, 'gold' : 'rush' : 1, 'town' : 1, 'tucked' : 1, ' sierra' : 1, 'nevada' : 1, 'foothills' : 1, 'lies' : 1, 'miles' : 2, ' north' : 1, 'sacramento' : 1, 'and' : 2, 'less' : 1, 'than' : 1, 'here' : 1, 'sister' : 1, 'communities' : 1, 'connected' : 1, 'skyway' : 1, 'four' : 1, 'lane' : 1, 'winding' : 1, 'road' : 1, 'its' :1, 'daily' : 1, 'commute' : 1, 'thousands' : 1, 'sole' : 1, 'route' : 1, 'out' : 1, 'into' : 1, 'chico' : 1, 'nearest' : 1, 'urban' : 1, 'area' : 1 })
+
+wordIndex = { 'first' :
+                ( 1,
+                    ['document_1122']),
+              'time' :
+                ( 1,
+                    ['document_1122']),
+              'google':
+                ( 1,
+                    ['document_1122']),
+                ( 1,
+              'photos'
+                    ['document_1122']),
+                ( 1,
+              'made'
+                ( 1,
+                    ['document_1122']),
+              'me' :
+                ( 2,
+                    ['document_1122']),
+              'cry' :
+                ( 1,
+                    ['document_1122']),
+              'sucker' :
+                ( 1,
+                    ['document_1122']),
+              'punch' :
+                ( 1,
+                    ['document_1122']),
+              'had' :
+                ( 2,
+                    ['document_1122']),
+              'looked' :
+                ( 1,
+                    ['document_1122']),
+              'my' :
+                ( 2,
+                    ['document_1122']),
+              'phone' :
+                ( 1,
+                    ['document_1122']),
+              'one' :
+                ( 1,
+                    ['document_1122']),
+              'morning' :
+                ( 1,
+                    ['document_1122']),
+              'april' :
+                ( 1,
+                    ['document_1122']),
+              'expecting' :
+                ( 1,
+                    ['document_1122']),
+              'more' :
+                ( 1,
+                    ['document_1122']),
+              'news' :
+                ( 1,
+                    ['document_1122']),
+              'global' :
+                ( 1,
+                    ['document_1122']),
+              'woe' :
+                ( 1,
+                    ['document_1122']),
+              'instead' :
+                ( 1,
+                    ['document_1122']),
+              'there' :
+                ( 1,
+                    ['document_1122']),
+              'alert' :
+                ( 1,
+                    ['document_1122']),
+              'photos' :
+                ( 1,
+                    ['document_1122']),
+              'letting' :
+                ( 1,
+                    ['document_1122']),
+              'know' :
+                ( 1,
+                    ['document_1122']),
+              'googles' :
+                ( 1,
+                    ['document_1122']),
+              'image-processing' :
+                ( 1,
+                    ['document_1122']),
+              'robots' :
+                ( 1,
+                    ['document_1122']),
+              'created' :
+                ( 1,
+                    ['document_1122']),
+              'some' :
+                ( 1,
+                    ['document_1122']),
+              'kind' :
+                ( 1,
+                    ['document_1122']),
+              'montage' :
+                ( 1,
+                    ['document_1122']),
+              'videos' :
+                ( 1,
+                    ['document_1122']),
+              'attract' :
+                ( 1,
+                    ['document_3546']),
+              'amazon' :
+                ( 2,
+                    ['document_3546']),
+              'new' :
+                ( 1,
+                    ['document_3546']),
+              'york' :
+                ( 1,
+                    ['document_3546']),
+              'city' :
+                ( 2,
+                    ['document_3546']),
+              'leaders' :
+                ( 1,
+                    ['document_3546']),
+              'agreed' :
+                ( 1,
+                    ['document_3546']),
+              'remake' :
+                ( 1,
+                    ['document_3546']),
+              'plans' :
+                ( 1,
+                    ['document_3546']),
+              'queens' :
+                ( 1,
+                    ['document_3546']),
+              'waterfront' :
+                ( 1,
+                    ['document_3546']),
+              'move' :
+                ( 1,
+                    ['document_3546']),
+              'distribution' :
+                ( 1,
+                    ['document_3546']),
+              'center' :
+                ( 1,
+                    ['document_3546']),
+              'school' :
+                ( 1,
+                    ['document_3546']),
+              'lunches' :
+                ( 1,
+                    ['document_3546']),
+              'provide' :
+                ( 1,
+                    ['document_3546']),
+              'sweeping' :
+                ( 1,
+                    ['document_3546']),
+              'package' :
+                ( 1,
+                    ['document_3456']),
+              'billion' :
+                ( 1,
+                    ['document_3546']),
+              'incentives' :
+                ( 1,
+                    ['document_3546']),
+              'state' :
+                ( 1,
+                    ['document_3546']),
+              'and' :
+                ( 1,
+                    ['document_3546']),
+              'hundreds' :
+                ( 1,
+                    ['document_3546']),
+              'millions' :
+                ( 1,
+                    ['document_3546']),
+              'more' :
+                ( 1,
+                    ['document_3546']),
+              'paradise' :
+                ( 2,
+                    ['document_7788']),
+              'old' :
+                ( 1,
+                    ['document_7788']),
+              'gold' :
+                ( 1,
+                    ['document_7788']),
+              'rush' :
+                ( 1,
+                    ['document_7788']),
+              'town' :
+                ( 1,
+                    ['document_7788']),
+              'tucked' :
+                ( 1,
+                    ['document_7788']),
+              'sierra' :
+                ( 1,
+                    ['document_7788']),
+              'nevada' :
+                ( 1,
+                    ['document_7788']),
+              'foothills' :
+                ( 1,
+                    ['document_7788']),
+              'lies' :
+                ( 1,
+                    ['document_7788']),
+              'miles' :
+                ( 2,
+                    ['document_7788']),
+              'north' :
+                ( 1,
+                    ['document_7788']),
+              'sacramento' :
+                ( 1,
+                    ['document_7788']),
+              'and' :
+                ( 2,
+                    ['document_7788']),
+              'less' :
+                ( 1,
+                    ['document_7788']),
+              'than' :
+                ( 1,
+                    ['document_7788']),
+              'here' :
+                ( 1,
+                    ['document_7788']),
+              'sister' :
+                ( 1,
+                    ['document_7788']),
+              'communities' :
+                ( 1,
+                    ['document_7788']),
+              'connected' :
+                ( 1,
+                    ['document_7788']),
+              'skyway' :
+                ( 1,
+                    ['document_7788']),
+              'four' :
+                ( 1,
+                    ['document_7788']),
+              'lane' :
+                ( 1,
+                    ['document_7788']),
+              'winding' :
+                ( 1,
+                    ['document_7788']),
+              'road' :
+                ( 1,
+                    ['document_7788']),
+              'its' :
+                ( 1,
+                    ['document_7788']),
+              'daily' :
+                ( 1,
+                    ['document_7788']),
+              'commute' :
+                ( 1,
+                    ['document_7788']),
+              'thousands' :
+                ( 1,
+                    ['document_7788']),
+              'sole' :
+                ( 1,
+                    ['document_7788']),
+              'route' :
+                ( 1,
+                    ['document_7788']),
+              'out' :
+                ( 1,
+                    ['document_7788']),
+              'into' :
+                ( 1,
+                    ['document_7788']),
+              'chico' :
+                ( 1,
+                    ['document_7788']),
+              'nearest' :
+                ( 1,
+                    ['document_7788']),
+              'urban' :
+                ( 1,
+                    ['document_7788']),
+              'area' :
+                ( 1,
+                    ['document_7788']),
+
+
+
+
+import celtsats as cs
+import os
+
+def addToDoc(doc_name, count):
+    cs.parser.documentIndex[doc_name]= ()
+    t= (count,{})
+    cs.parser.documentIndex[doc_name]= t
+
+def addToInsideDoc(doc_name, stream):
+    for word in stream:
+        count= cs.parser.documentIndex[doc_name][0]
+        d2= cs.parser.documentIndex[doc_name][1]
+        count +=1
+        if word in d2:
+            count2= d2[word]
+            d2[word]= count2 + 1
+        else:
+            d2[word]= 1
+        tuple=(count, d2)
+        cs.parser/documentIndex[doc_name]= tuple
+
+    def readFiles(filesPath):
+        fs= os,listdir(filesPath)
+            for file in fs:
+            print('input file is:' + file)
+            addToDoc(file, 0)
+
+            with open(filesPath + file, encoding='ISO-8859-1') as f:
+                for line in f:
+                    line2= line.split()
+                    addtoInsideDoc(file, line2)
+
+
 #for
 #def buildDiction(document, words):
 #    for w in words :''
