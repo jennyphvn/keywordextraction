2d1efa2d56fee471ce00b1438ff48b6e432ae892
Katherine Hasker <haskerk@stthom.edu>
2018-12-06 20:03:05 -0600

    7, 8, 9, 10

--- celtstats/stats/stats.py
+++ celtstats/stats/stats.py
@@ -31,7 +31,7 @@ def maxValue(values) :
 
     return max
 
-def freWordsDoc(document, word):
+def freqWordsDoc(document, word):
     count = 0
     f = open('document_1122.txt','r')
     doc = f.readlines()
@@ -97,4 +97,71 @@ def wordEndLetterDoc(document, word):
     for i in range(len(doc)):
          endList.append(doc[i].strip('\n'))
     f.close()
+    return endList
+
+def freqDocs(word): #given a word, return a list of the documents where that word appears
+    f = open('document_1122.txt','r')
+    doc = f.readlines()
+    for line in doc:
+        if 'more' in line:
+            print("document_1122")
+    f.close()
+    f = open('document_3456.txt','r')
+    doc = f.readlines()
+    for line in doc:
+        if 'more' in line:
+            print("document_3456")
+    f.close()
+    f = open('document_7788.txt','r')
+    doc = f.readlines()
+    for line in doc:
+        if 'more' in line:
+            print("document_7788")
+    f.close()
+
+def freqDocsWords(count, k): #given a count, return a list of documents that have that number of words, because the
+                             #result can be long, return only k documents (where k is an integer)
+    f = open('document_1122.txt','r')
+    doc = f.readlines()
+    if len(doc) = 56:
+        print("document_1122")
+    f.close()
+    f = open('document_3456.txt','r')
+    doc = f.readlines()
+    if len(doc) = 56:
+            print("document_3456")
+    f.close()
+    f = open('document_7788.txt','r')
+    doc = f.readlines()
+    if len(doc) = 56:
+            print("document_7788")
+    f.close()
+
+def freqWordsSizeDoc(document, word): #given a document name, return the result of the number of times that word
+                                      #appear in that document divided by the number of unique words in that document.
+    endList = {}
+    count = 0
+    f = open('document_1122.txt','r')
+    doc = f.readlines()
+    for line in doc:
+        if 'google' in line:
+            count += 1
+    f.close()
+    division = (count / len(doc))
+    print(division)
+    return endList
+
+def augmentedCountWord(document, word): #given a document and a word, return the following computation: 0.5 +
+                                        # (0.5 * number of times that word appears in the document/number of times of the most common word in that
+                                        # document).
+    endList = {}
+    count = 0
+    f = open('document_1122.txt','r')
+    doc = f.readlines()
+    for line in doc:
+        if 'google' in line:
+            count += 1
+    f.close()
+    aug = (0.5 + (0.5 * count))
+    print(aug)
     return endList
